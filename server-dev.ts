323b4f8889fe7fdc47f87ec256f1b469c749969f
Zack Ames <zames6584@gmail.com>
2024-01-31 13:13:57 -0500

    refactor

--- server-dev.ts
+++ server-dev.ts
@@ -2,16 +2,9 @@ import fs from 'fs';
 import express from 'express';
 import { createServer } from 'vite';
 import { getFrameAccountAddress } from '@coinbase/onchainkit';
-import get_canvas from './frame-canvas.js';
+import {IFrameProps, frameGenerator} from './frames';
 
 const app = express();
-
-interface IFrameProps {
-  frame?: string;
-  imageUrl: string;
-  buttons?: string[];
-  postUrl?: string;
-}
  
 const vite = await createServer({
   server: {
@@ -41,76 +34,42 @@ app.use('/', async (req, res) => {
   }
 });
  
-function generateFarcasterFrameMetaTag({ frame, imageUrl, postUrl, buttons }: IFrameProps): string {
-  // Default to vNext
-  if (!frame) {
-      frame = "vNext"
+app.get('/frame', async (req, res) => {
+
+  let data;
+  try {
+    const { getServerData } = await vite.ssrLoadModule('/src/function.ts');
+    const data = await getServerData();
+  } catch (error) {
+    res.status(500).end('error');
   }
-  // Ensure there are at most four buttons
-  if (buttons && buttons.length > 4) {
-      throw new Error("Maximum of four buttons are allowed per frame.");
-  }
-
-  // Generate Open Graph tags for image, redirect, and buttons
-  let metaTag = `<meta property="fc:frame" content="${frame ? frame : "vNext"}" />\n`;
-  metaTag += `<meta property="fc:frame:image" content="${imageUrl}" />\n`;
-
-  if (buttons) {
-      buttons.forEach((button, index) => {
-          metaTag += `<meta property="fc:frame:button:${index + 1}" content="${button}" />\n`;
-      });
-  }
-
-  // post URL if exists
-  if (postUrl) {
-      metaTag += `<meta property="fc:frame:post_url" content="${postUrl}" /> \n`
-  }
-
-  return metaTag;
-}
-
-async function frameGenerator(frameProps: IFrameProps): Promise<string> {
-
-  const metaTag = generateFarcasterFrameMetaTag(frameProps);
-
-
-  const { getServerData } = await import('./dist/function/function.js');
-  const data = await getServerData();
-
-  const img = get_canvas(data);
-
-  const html = `<!DOCTYPE html>
-      <html>
-          <head>
-              <meta charset="utf-8">
-              <title>NHL Scores</title>
-              <meta property="og:title" content="NHLScores" />
-              <meta property="og:image" content="${img}" />
-              ${metaTag}
-          </head>
-      </html>
-  `;
-  return html;
-}
-
-app.get('/frame', (req, res) => {
 
   const frameProps: IFrameProps = {
-      imageUrl: 'https://i.imgur.com/osEKmhB.png',
-      buttons: ['get', 'button2'],
+    data: data,
+    buttons: ['get', 'button2'],
   };
+
+
+  
 
   res.status(200).send(frameGenerator(frameProps));
 });
 
-app.post('/frame', (req, res) => {
+app.post('/frame', async (req, res) => {
 
   console.log(req.body)
 
-  const frameProps: IFrameProps = {
-      imageUrl:  'https://i.imgur.com/osEKmhB.png',
-      buttons: ['post', 'button2'],
+  let data;
+  try {
+    const { getServerData } = await vite.ssrLoadModule('/src/function.ts');
+    const data = await getServerData();
+  } catch (error) {
+    res.status(500).end('error');
+  }
 
+  const frameProps: IFrameProps = {
+    data: data,
+    buttons: ['get', 'button2'],
   };
   
   res.status(200).send(frameGenerator(frameProps));
--- server-prod.ts
+++ server-prod.ts
@@ -2,16 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import get_canvas from './frame-canvas.js';
+import { frameGenerator, IFrameProps } from './frames.js';
  
 const app = express();
- 
-interface IFrameProps {
-  frame?: string;
-  imageUrl: string;
-  buttons?: string[];
-  postUrl?: string;
-}
 
 app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client'), { index: false }));
 
@@ -34,76 +27,39 @@ app.use('/', async (_, res) => {
   }
 });
 
-function generateFarcasterFrameMetaTag({ frame, imageUrl, postUrl, buttons }: IFrameProps): string {
-  // Default to vNext
-  if (!frame) {
-      frame = "vNext"
+app.get('/frame', async (req, res) => {
+
+  let data;
+  try {
+    const { getServerData } = await import('./dist/function/function.js');
+    const data = await getServerData();
+  } catch (error) {
+    res.status(500).end('error');
   }
-  // Ensure there are at most four buttons
-  if (buttons && buttons.length > 4) {
-      throw new Error("Maximum of four buttons are allowed per frame.");
-  }
-
-  // Generate Open Graph tags for image, redirect, and buttons
-  let metaTag = `<meta property="fc:frame" content="${frame ? frame : "vNext"}" />\n`;
-  metaTag += `<meta property="fc:frame:image" content="${imageUrl}" />\n`;
-
-  if (buttons) {
-      buttons.forEach((button, index) => {
-          metaTag += `<meta property="fc:frame:button:${index + 1}" content="${button}" />\n`;
-      });
-  }
-
-  // post URL if exists
-  if (postUrl) {
-      metaTag += `<meta property="fc:frame:post_url" content="${postUrl}" /> \n`
-  }
-
-  return metaTag;
-}
-
-async function frameGenerator(frameProps: IFrameProps): Promise<string> {
-
-  const metaTag = generateFarcasterFrameMetaTag(frameProps);
-
-
-  const { getServerData } = await import('./dist/function/function.js');
-  const data = await getServerData();
-
-  const img = get_canvas(data);
-
-  const html = `<!DOCTYPE html>
-      <html>
-          <head>
-              <meta charset="utf-8">
-              <title>NHL Scores</title>
-              <meta property="og:title" content="NHLScores" />
-              <meta property="og:image" content="${img}" />
-              ${metaTag}
-          </head>
-      </html>
-  `;
-  return html;
-}
-
-app.get('/frame', (req, res) => {
 
   const frameProps: IFrameProps = {
-      imageUrl: 'https://i.imgur.com/osEKmhB.png',
-      buttons: ['get', 'button2'],
+    data: data,
+    buttons: ['get', 'button2'],
   };
 
   res.status(200).send(frameGenerator(frameProps));
 });
 
-app.post('/frame', (req, res) => {
+app.post('/frame', async (req, res) => {
 
   console.log(req.body)
 
-  const frameProps: IFrameProps = {
-      imageUrl:  'https://i.imgur.com/osEKmhB.png',
-      buttons: ['post', 'button2'],
+  let data;
+  try {
+    const { getServerData } = await import('./dist/function/function.js');
+    const data = await getServerData();
+  } catch (error) {
+    res.status(500).end('error');
+  }
 
+  const frameProps: IFrameProps = {
+    data: data,
+    buttons: ['get', 'button2'],
   };
   
   res.status(200).send(frameGenerator(frameProps));
