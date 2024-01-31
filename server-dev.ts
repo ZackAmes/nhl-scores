750d684ca873ea5f5b18f3e30794f8cb154a47ad
Zack Ames <zames6584@gmail.com>
2024-01-31 12:45:03 -0500

    start node canvas

--- server-dev.ts
+++ server-dev.ts
@@ -2,9 +2,16 @@ import fs from 'fs';
 import express from 'express';
 import { createServer } from 'vite';
 import { getFrameAccountAddress } from '@coinbase/onchainkit';
+import get_canvas from './frame-canvas.js';
 
- 
 const app = express();
+
+interface IFrameProps {
+  frame?: string;
+  imageUrl: string;
+  buttons?: string[];
+  postUrl?: string;
+}
  
 const vite = await createServer({
   server: {
@@ -15,7 +22,7 @@ const vite = await createServer({
  
 app.use(vite.middlewares);
  
-app.use('*', async (req, res) => {
+app.use('/', async (req, res) => {
   const url = req.originalUrl;
  
   try {
@@ -34,6 +41,81 @@ app.use('*', async (req, res) => {
   }
 });
  
+function generateFarcasterFrameMetaTag({ frame, imageUrl, postUrl, buttons }: IFrameProps): string {
+  // Default to vNext
+  if (!frame) {
+      frame = "vNext"
+  }
+  // Ensure there are at most four buttons
+  if (buttons && buttons.length > 4) {
+      throw new Error("Maximum of four buttons are allowed per frame.");
+  }
+
+  // Generate Open Graph tags for image, redirect, and buttons
+  let metaTag = `<meta property="fc:frame" content="${frame ? frame : "vNext"}" />\n`;
+  metaTag += `<meta property="fc:frame:image" content="${imageUrl}" />\n`;
+
+  if (buttons) {
+      buttons.forEach((button, index) => {
+          metaTag += `<meta property="fc:frame:button:${index + 1}" content="${button}" />\n`;
+      });
+  }
+
+  // post URL if exists
+  if (postUrl) {
+      metaTag += `<meta property="fc:frame:post_url" content="${postUrl}" /> \n`
+  }
+
+  return metaTag;
+}
+
+async function frameGenerator(frameProps: IFrameProps): Promise<string> {
+
+  const metaTag = generateFarcasterFrameMetaTag(frameProps);
+
+
+  const { getServerData } = await import('./dist/function/function.js');
+  const data = await getServerData();
+
+  const img = get_canvas(data);
+
+  const html = `<!DOCTYPE html>
+      <html>
+          <head>
+              <meta charset="utf-8">
+              <title>NHL Scores</title>
+              <meta property="og:title" content="NHLScores" />
+              <meta property="og:image" content="${img}" />
+              ${metaTag}
+          </head>
+      </html>
+  `;
+  return html;
+}
+
+app.get('/frame', (req, res) => {
+
+  const frameProps: IFrameProps = {
+      imageUrl: 'https://i.imgur.com/osEKmhB.png',
+      buttons: ['get', 'button2'],
+  };
+
+  res.status(200).send(frameGenerator(frameProps));
+});
+
+app.post('/frame', (req, res) => {
+
+  console.log(req.body)
+
+  const frameProps: IFrameProps = {
+      imageUrl:  'https://i.imgur.com/osEKmhB.png',
+      buttons: ['post', 'button2'],
+
+  };
+  
+  res.status(200).send(frameGenerator(frameProps));
+});
+
 app.listen(4173, () => {
   console.log('http://localhost:4173.');
 });
--- server-prod.ts
+++ server-prod.ts
@@ -2,12 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
+import get_canvas from './frame-canvas.js';
  
 const app = express();
  
+interface IFrameProps {
+  frame?: string;
+  imageUrl: string;
+  buttons?: string[];
+  postUrl?: string;
+}
+
 app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client'), { index: false }));
- 
-app.use('*', async (_, res) => {
+
+app.use('/', async (_, res) => {
   try {
     const template = fs.readFileSync('./dist/client/index.html', 'utf-8');
     const { render } = await import('./dist/server/entry-server.js');
@@ -24,6 +32,81 @@ app.use('*', async (_, res) => {
   } catch (error) {
     res.status(500).end(error);
   }
+});
+
+function generateFarcasterFrameMetaTag({ frame, imageUrl, postUrl, buttons }: IFrameProps): string {
+  // Default to vNext
+  if (!frame) {
+      frame = "vNext"
+  }
+  // Ensure there are at most four buttons
+  if (buttons && buttons.length > 4) {
+      throw new Error("Maximum of four buttons are allowed per frame.");
+  }
+
+  // Generate Open Graph tags for image, redirect, and buttons
+  let metaTag = `<meta property="fc:frame" content="${frame ? frame : "vNext"}" />\n`;
+  metaTag += `<meta property="fc:frame:image" content="${imageUrl}" />\n`;
+
+  if (buttons) {
+      buttons.forEach((button, index) => {
+          metaTag += `<meta property="fc:frame:button:${index + 1}" content="${button}" />\n`;
+      });
+  }
+
+  // post URL if exists
+  if (postUrl) {
+      metaTag += `<meta property="fc:frame:post_url" content="${postUrl}" /> \n`
+  }
+
+  return metaTag;
+}
+
+async function frameGenerator(frameProps: IFrameProps): Promise<string> {
+
+  const metaTag = generateFarcasterFrameMetaTag(frameProps);
+
+
+  const { getServerData } = await import('./dist/function/function.js');
+  const data = await getServerData();
+
+  const img = get_canvas(data);
+
+  const html = `<!DOCTYPE html>
+      <html>
+          <head>
+              <meta charset="utf-8">
+              <title>NHL Scores</title>
+              <meta property="og:title" content="NHLScores" />
+              <meta property="og:image" content="${img}" />
+              ${metaTag}
+          </head>
+      </html>
+  `;
+  return html;
+}
+
+app.get('/frame', (req, res) => {
+
+  const frameProps: IFrameProps = {
+      imageUrl: 'https://i.imgur.com/osEKmhB.png',
+      buttons: ['get', 'button2'],
+  };
+
+  res.status(200).send(frameGenerator(frameProps));
+});
+
+app.post('/frame', (req, res) => {
+
+  console.log(req.body)
+
+  const frameProps: IFrameProps = {
+      imageUrl:  'https://i.imgur.com/osEKmhB.png',
+      buttons: ['post', 'button2'],
+
+  };
+  
+  res.status(200).send(frameGenerator(frameProps));
 });
  
 app.listen(5173, () => {
