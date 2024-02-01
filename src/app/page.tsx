5bcf66a8912fa1445f739fe695ddcf237ac14fa6
Zack Ames <zames6584@gmail.com>
2024-01-31 22:12:06 -0500

    test fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,27 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
-const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
+const NEXT_PUBLIC_URL = 'https://nhl-scores.vercel.app/';
 
 const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Click Me',
-    },
-  ],
-  image: `${NEXT_PUBLIC_URL}/park-1.png`,
-  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+  buttons: [{label: 'Farcaster'}, {label: 'Twitter'}],
+  image: 'https://first-frame-phi.vercel.app/choice.jpg',
+  post_url: 'https:///first-frame-phi.vercel.app/api/frame',
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
+  title: '',
+  description: 'choose',
   openGraph: {
-    title: 'zizzamia.xyz',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    title: 'choose',
+    description: 'pick a path',
+    images: ['https://first-frame-phi.vercel.app/lightpath.jpg', 'https://first-frame-phi.vercel.app/darkpath.png'],
   },
   other: {
     ...frameMetadata,
   },
 };
+
 
 export default function Page() {
   return (
