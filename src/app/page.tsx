27672f81410aba42330777b9c65cae38009852e2
Zack Ames <zames6584@gmail.com>
2024-01-31 22:05:39 -0500

    try fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,15 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: `${NEXT_PUBLIC_URL}/park-1.png`,
+  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
-  title: 'nhl scores',
+  title: 'zizzamia.xyz',
+  description: 'LFG',
   openGraph: {
+    title: 'zizzamia.xyz',
+    description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
